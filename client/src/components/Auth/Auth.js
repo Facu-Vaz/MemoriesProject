46beeeec024bb5a75018e5875a6760ca2c5ab4d2
Facu-Vaz <facundo.vazber@gmail.com>
2022-12-10 18:47:57 -0300

    Initial commit

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Avatar, Button, Paper, Grid, Typography } from '@material-ui/core';
+
+const Auth = () => {
+  const state = null;
+  
+    return (
+    <div>Auth</div>
+  )
+}
+
+export default Auth
